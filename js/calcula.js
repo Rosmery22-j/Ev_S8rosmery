8f31bca8afc39b0a5f0c7ff05f4abc539545d426
Rosmery22-j <rosmerypalominotampi@gmail.com>
2020-10-01 14:30:05 -0500

    version 1

---
+++ js/calcula.js
@@ -0,0 +1,67 @@
+var aun="";
+
+function borrar (){
+    aler(borrar);
+    document.getElementById ("borrar").value=aun;
+}
+function siete() {
+    //aler(siete);
+    aun=aun+7;
+    document.getElementById ("ingreso").value=aun;
+
+}
+
+function ocho() {
+   // aler(ocho);
+    aun=aun+8;
+    document.getElementById ("ingreso").value=aun;
+}
+
+function nueve() {
+    //aler(nueve);
+    aun=aun+9;
+    document.getElementById ("ingreso").value=aun;
+}
+
+function cuatro() {
+   // aler(cuatro);
+    aun=aun+4;
+    document.getElementById ("ingreso").value=aun;
+}
+
+function cinco() {
+    //aler(cinco);
+    aun=aun+4;
+    document.getElementById ("ingreso").value=aun;
+}
+
+function seis() {
+    //aler(seis);
+    aun=aun+6;
+    document.getElementById ("ingreso").value=aun;
+}
+
+function uno() {
+    //aler(uno);
+    aun=aun+1;
+    document.getElementById ("ingreso").value=aun;
+}
+
+function dos() {
+    //aler(dos);
+    aun=aun+2;
+    document.getElementById ("ingreso").value=aun;
+}
+
+function tres() {
+    //aler(tres);
+    aun=aun+3;
+    document.getElementById ("ingreso").value=aun;
+}
+
+function cero() {
+    //aler(cero);
+    aun=aun+0;
+    document.getElementById ("ingreso").value=aun;
+}
+
